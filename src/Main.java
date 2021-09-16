054e050b9b90abb125568131fd1e1e86d7ebc106
Victor S <victor.skarerverket@gmail.com>
2021-09-16 15:03:04 +0200

    kommentar

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Growing a branch.");
-        System.out.println("Growing a second branch");
+
+        System.out.println("i want a damn tree.");
     }
 }

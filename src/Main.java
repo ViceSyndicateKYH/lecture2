6ddf21f4503c1b4fe41e871b3bcebf0c0a4584b1
Victor S <victor.skarerverket@gmail.com>
2021-09-16 15:05:26 +0200

    extending master

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Drink Whiskey & Coke");
+
+        System.out.println("updating master branch");
     }
 }

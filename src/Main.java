c239299f7d965d7ff7124ac3a64153c2a3a49461
Victor S <victor.skarerverket@gmail.com>
2021-09-16 14:53:58 +0200

    third branch grown

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Growing a branch.");
+        System.out.println("Growing a branch");
         System.out.println("Growing a second branch");
+        System.out.println("Growing a third branch");
     }
 }
